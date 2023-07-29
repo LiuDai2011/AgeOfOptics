d99adf9a73581562e4aef7ca8bd78211d47376c1
LiuDai2011 <135331340+LiuDai2011@users.noreply.github.com>
2023-07-29 20:19:25 +0800

    alpha 0.0.2

--- src/AOO/content/AOOItems.java
+++ src/AOO/content/AOOItems.java
@@ -6,6 +6,16 @@ public class AOOItems {
     public static Item quantumSyntheticGold, ionicAlloys, opticalMixture;
 
     public static void load() {
+        quantumSyntheticGold = new Item("quantum-synthetic-gold"){{
+            cost = 1f;
+        }};
 
+        ionicAlloys = new Item("ionic-alloys"){{
+            cost = 1f;
+        }};
+
+        opticalMixture = new Item("optical-mixture"){{
+            cost = 1f;
+        }};
     }
 }
